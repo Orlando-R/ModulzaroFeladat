3784f9ae8ad98dd278dcbeeb8d751ba02b013676
Orlando-R <roglorand@gmail.com>
2021-04-14 18:29:08 +0200

    1. feladat -összesités

---
+++ Balosok.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulzaro0414
+{
+    class Balosok
+    {
+        public string nev { get; set; }
+        public string elso { get; set; }
+        public string utolso { get; set; }
+        public double suly { get; set; }
+        public double magassag { get; set; }
+
+        public Balosok(string soronkent)
+        {
+            string[] sor = soronkent.Split(';');
+            this.nev = sor[0];
+            this.elso = sor[1];
+            this.utolso = sor[2];
+            this.suly = int.Parse(sor[3]);
+            this.magassag = int.Parse(sor[4]);
+        }
+    }
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulzaro0414
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] soronkent = File.ReadAllLines("balkezesek.csv");
+            List<Balosok> balosSportolok = new List<Balosok>();
+
+            foreach (string sor in soronkent.Skip(1))
+            {
+                balosSportolok.Add(new Balosok(sor));
+            }
+            //1 feldat
+            int N = balosSportolok.Count;
+            Console.WriteLine($"1.feladat: {N} versenyző");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
